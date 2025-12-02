e3c967c82cad5f2532aff7c5801677dbb9ae9e59
Dev621-tech <devinosborne11@yahoo.com>
2025-12-02 17:16:09 -0500

    Completed the Arithmetic Chain

--- script.js
+++ script.js
@@ -39,7 +39,34 @@ console.log(isValid);
 // but it SHOULD NOT be done this way. As programmers,
 // we break things into small, manageable pieces so that
 // they can be better understood, scaled, and maintained.
-const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
-  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
+    ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) &&
+    !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) &&
+    (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+  // ---------------------------------------------------------------------
+
+// Check if all numbers are divisible by 5. Cache the result in a variable.
+const divBy5 = (n1 / 5 )
+
+// Check if the first number is larger than the last.Cache the result in a variable.
+const largerNum = (n1 > n4)
+console.log(largerNum)
+
+// Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+
+const theResult = (n1 - n2)
+console.log(theResult)
+
+// Multiply the result by the third number.
+
+let theAnswer = (theResult * n3)
+console.log(theAnswer)
+
+// Find the remainder of dividing the result by the fourth number.
+
+let theRemainder = (theAnswer / n4)
+console.log(theRemainder)
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator(!) in other logic comparisons.Rename the variable as appropriate.
